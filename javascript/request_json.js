49cad5d47f84d1cd421c2766676d3e692db8bb17
Alan Martins Meirelles <50336705+alanmmeirelles@users.noreply.github.com>
2020-01-27 18:08:59 -0300

    Final
    - Site foi finalizado, como foi na forma de metodologia ágil, fiz o layout da forma mais breve que pude, dei o meu máximo em um tempo recorde.
    
    - Como tive problema de CORS a solução que achei foi fazer arquivos JS com os JSON recebidos
    
    - Sei que essa repetição para inputar as informações deveria ser feito com uma estrutura de repetição, mas como sou novato iria levar dias p desenvolver essa função, então fiz da forma que dava no tempo que tinha.

--- javascript/request_json.js
+++ javascript/request_json.js
@@ -44,9 +44,6 @@
   //fim  da área para carregamento de listas //
 
 
-
-
-
 //inicio da área para bindar o conteudo no html //
 
         function bindBanner(banner){  //função para inserir informações do json banner.js no html//
@@ -55,32 +52,107 @@
           var TituloBanner1 = document.querySelector("[data-banner1='banner1Titulo']");
           TituloBanner1.innerHTML = banner[1].titulo;
         }
+
         function bindCloud(cloud){  //função para inserir informações do json cloud.js no html//
-          var cloudtag0 = document.querySelector("[data-cloudtag0='cloudtag']");
-          cloudtag0.innerHTML = cloud[0].tag;
-          var cloudtag1 = document.querySelector("[data-cloudtag1='cloudtag']");
-          cloudtag1.innerHTML = cloud[1].tag;
-          var cloudtag2 = document.querySelector("[data-cloudtag2='cloudtag']");
-          cloudtag2.innerHTML = cloud[2].tag;
-          var cloudtag3 = document.querySelector("[data-cloudtag3='cloudtag']");
-          cloudtag3.innerHTML = cloud[3].tag;
-          var cloudtag4 = document.querySelector("[data-cloudtag4='cloudtag']");
-          cloudtag4.innerHTML = cloud[4].tag;
-          var cloudtag5 = document.querySelector("[data-cloudtag5='cloudtag']");
-          cloudtag5.innerHTML = cloud[5].tag;
-          var cloudtag6 = document.querySelector("[data-cloudtag6='cloudtag']");
-          cloudtag6.innerHTML = cloud[6].tag;
+          var Cloudtag0 = document.querySelector("[data-cloudtag0='cloudtag']");
+          Cloudtag0.innerHTML = cloud[0].tag;
+          var Cloudtag1 = document.querySelector("[data-cloudtag1='cloudtag']");
+          Cloudtag1.innerHTML = cloud[1].tag;
+          var Cloudtag2 = document.querySelector("[data-cloudtag2='cloudtag']");
+          Cloudtag2.innerHTML = cloud[2].tag;
+          var Cloudtag3 = document.querySelector("[data-cloudtag3='cloudtag']");
+          Cloudtag3.innerHTML = cloud[3].tag;
+          var Cloudtag4 = document.querySelector("[data-cloudtag4='cloudtag']");
+          Cloudtag4.innerHTML = cloud[4].tag;
+          var Cloudtag5 = document.querySelector("[data-cloudtag5='cloudtag']");
+          Cloudtag5.innerHTML = cloud[5].tag;
+          var Cloudtag6 = document.querySelector("[data-cloudtag6='cloudtag']");
+          Cloudtag6.innerHTML = cloud[6].tag;
         }
 
+        function bindNoticias(noticia){  //função para inserir informações do json noticia.js no html//
+          var TituloNot0 = document.querySelector("[data-TituloNoticia0='tit_noticias']");
+          var ResumoNot0 = document.querySelector("[data-ResumoNoticia0='res_noticias']");
+          TituloNot0.innerHTML = noticia[0].titulo;
+          ResumoNot0.innerHTML = noticia[0].sumario;
+          var TituloNot1 = document.querySelector("[data-TituloNoticia1='tit_noticias']");
+          var ResumoNot1 = document.querySelector("[data-ResumoNoticia1='res_noticias']");
+          TituloNot1.innerHTML = noticia[1].titulo;
+          ResumoNot1.innerHTML = noticia[1].sumario;
+          var TituloNot2 = document.querySelector("[data-TituloNoticia2='tit_noticias']");
+          var ResumoNot2 = document.querySelector("[data-ResumoNoticia2='res_noticias']");
+          TituloNot2.innerHTML = noticia[2].titulo;
+          ResumoNot2.innerHTML = noticia[2].sumario;
+          var TituloNot3 = document.querySelector("[data-TituloNoticia3='tit_noticias']");
+          var ResumoNot3 = document.querySelector("[data-ResumoNoticia3='res_noticias']");
+          TituloNot3.innerHTML = noticia[3].titulo;
+          ResumoNot3.innerHTML = noticia[3].sumario;
+        }
+
+        function bindVitrine(vitrine){  //função para inserir informações do json vitrine.js no html//
+          var TipoVit0 = document.querySelector("[data-tipoVitrine0='tipo_vit']");
+          var BairroVit0 = document.querySelector("[data-bairroVitrine0='bairro_vit']");
+          var QuartosVit0 = document.querySelector("[data-quartosVitrine0='quartos_vit']");
+          var VagasVit0 = document.querySelector("[data-vagasVitrine0='vagas_vit']");
+          var MetroVit0 = document.querySelector("[data-metrosVitrine0='metros_vit']");
+          TipoVit0.innerHTML = vitrine[0].tipo;
+          BairroVit0.innerHTML = vitrine[0].bairro + ' - RJ';
+          QuartosVit0.innerHTML = vitrine[0].quartos;
+          VagasVit0.innerHTML = vitrine[0].vagas;
+          MetroVit0.innerHTML = vitrine[0].metragem;
+          var TipoVit1 = document.querySelector("[data-tipoVitrine1='tipo_vit']");
+          var BairroVit1 = document.querySelector("[data-bairroVitrine1='bairro_vit']");
+          var QuartosVit1 = document.querySelector("[data-quartosVitrine1='quartos_vit']");
+          var VagasVit1 = document.querySelector("[data-vagasVitrine1='vagas_vit']");
+          var MetroVit1 = document.querySelector("[data-metrosVitrine1='metros_vit']");
+          TipoVit1.innerHTML = vitrine[1].tipo;
+          BairroVit1.innerHTML = vitrine[1].bairro + ' - RJ';
+          QuartosVit1.innerHTML = vitrine[1].quartos;
+          VagasVit1.innerHTML = vitrine[1].vagas;
+          MetroVit1.innerHTML = vitrine[1].metragem;
+          var TipoVit2 = document.querySelector("[data-tipoVitrine2='tipo_vit']");
+          var BairroVit2 = document.querySelector("[data-bairroVitrine2='bairro_vit']");
+          var QuartosVit2 = document.querySelector("[data-quartosVitrine2='quartos_vit']");
+          var VagasVit2 = document.querySelector("[data-vagasVitrine2='vagas_vit']");
+          var MetroVit2 = document.querySelector("[data-metrosVitrine2='metros_vit']");
+          TipoVit2.innerHTML = vitrine[2].tipo;
+          BairroVit2.innerHTML = vitrine[2].bairro + ' - RJ';
+          QuartosVit2.innerHTML = vitrine[2].quartos;
+          VagasVit2.innerHTML = vitrine[2].vagas;
+          MetroVit2.innerHTML = vitrine[2].metragem;
+          var TipoVit3 = document.querySelector("[data-tipoVitrine3='tipo_vit']");
+          var BairroVit3 = document.querySelector("[data-bairroVitrine3='bairro_vit']");
+          var QuartosVit3 = document.querySelector("[data-quartosVitrine3='quartos_vit']");
+          var VagasVit3 = document.querySelector("[data-vagasVitrine3='vagas_vit']");
+          var MetroVit3 = document.querySelector("[data-metrosVitrine3='metros_vit']");
+          TipoVit3.innerHTML = vitrine[3].tipo;
+          BairroVit3.innerHTML = vitrine[3].bairro + ' - RJ';
+          QuartosVit3.innerHTML = vitrine[3].quartos;
+          VagasVit3.innerHTML = vitrine[3].vagas;
+          MetroVit3.innerHTML = vitrine[3].metragem;
+          var TipoVit4 = document.querySelector("[data-tipoVitrine4='tipo_vit']");
+          var BairroVit4 = document.querySelector("[data-bairroVitrine4='bairro_vit']");
+          var QuartosVit4 = document.querySelector("[data-quartosVitrine4='quartos_vit']");
+          var VagasVit4 = document.querySelector("[data-vagasVitrine4='vagas_vit']");
+          var MetroVit4 = document.querySelector("[data-metrosVitrine4='metros_vit']");
+          TipoVit4.innerHTML = vitrine[4].tipo;
+          BairroVit4.innerHTML = vitrine[4].bairro + ' - RJ';
+          QuartosVit4.innerHTML = vitrine[4].quartos;
+          VagasVit4.innerHTML = vitrine[4].vagas;
+          MetroVit4.innerHTML = vitrine[4].metragem;
+          var TipoVit5 = document.querySelector("[data-tipoVitrine5='tipo_vit']");
+          var BairroVit5 = document.querySelector("[data-bairroVitrine5='bairro_vit']");
+          var QuartosVit5 = document.querySelector("[data-quartosVitrine5='quartos_vit']");
+          var VagasVit5 = document.querySelector("[data-vagasVitrine5='vagas_vit']");
+          var MetroVit5 = document.querySelector("[data-metrosVitrine5='metros_vit']");
+          TipoVit5.innerHTML = vitrine[5].tipo;
+          BairroVit5.innerHTML = vitrine[5].bairro + ' - RJ';
+          QuartosVit5.innerHTML = vitrine[5].quartos;
+          VagasVit5.innerHTML = vitrine[5].vagas;
+          MetroVit5.innerHTML = vitrine[5].metragem;
+        }
 
 //fim da área para bindar o conteudo no html //
-
-
-
-
-
-
-
 
         //    FUNÇÃO DE REQUISIÇÃO   //
         function ajax(url,method,callback){    //criando a função pegando/passando os parametros Url, metodo e callback//
